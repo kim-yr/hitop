a2a8371c7cd5b0d777d114b2f724f965091efefb
kim-yr <dPfud1138@gmail.com>
2021-11-09 16:12:09 +0900

    main js 추가

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//swiper-wrapper의 부모 이름 적어야 함. id, class 둘 중 암거나
+const mainVisualSlider = new Swiper("#mainVisual", {
+  autoplay: {
+    delay: 2000, //머무르는 시간
+    disableOnInteraction: false,
+  },
+  speed: 1000, //넘어가는 시간
+  effect: "fade",
+  loop: true,
+  navigation: {
+    prevEl: "#mainVisual .prev",
+    nextEl: "#mainVisual .next",
+  },
+});
